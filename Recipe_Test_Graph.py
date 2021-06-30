eaa5092f4e7582353d329f7615bd7032b3f1380d
Umit <60366307+umitozmen@users.noreply.github.com>
2021-06-30 15:36:46 +0100

    Networkx Initial

--- Recipe_Test_Graph.py
+++ Recipe_Test_Graph.py
@@ -1,44 +1,16 @@
 from pathlib import Path
 #from mpl_toolkits.mplot3d import Axes3D
 #from sklearn.preprocessing import StandardScaler
-#import matplotlib.pyplot as plt # plotting
+import matplotlib.pyplot as plt # plotting
 #import numpy as np # linear algebra
 import os # accessing directory structure
 #import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-#import networkx as nx
+import networkx as nx
 
 def get_file_names():
     for dirname, _, filenames in os.walk('dataset'):
         for filename in filenames:
             print(os.path.join(dirname, filename))
-
-# Distribution graphs (histogram/bar graph) of column data
-def plotPerColumnDistribution(df, nGraphShown, nGraphPerRow):
-    nunique = df.nunique()
-    df = df[[col for col in df if nunique[col] > 1 and nunique[col] < 50]] # For displaying purposes, pick columns that have between 1 and 50 unique values
-    nRow, nCol = df.shape
-    columnNames = list(df)
-    nGraphRow = (nCol + nGraphPerRow - 1) / nGraphPerRow
-    plt.figure(num = None, figsize = (6 * nGraphPerRow, 8 * nGraphRow), dpi = 80, facecolor = 'w', edgecolor = 'k')
-    for i in range(min(nCol, nGraphShown)):
-        plt.subplot(nGraphRow, nGraphPerRow, i + 1)
-        columnDf = df.iloc[:, i]
-        if (not np.issubdtype(type(columnDf.iloc[0]), np.number)):
-            valueCounts = columnDf.value_counts()
-            valueCounts.plot.bar()
-        else:
-            columnDf.hist()
-        plt.ylabel('counts')
-        plt.xticks(rotation = 90)
-        plt.title(f'{columnNames[i]} (column {i})')
-    plt.tight_layout(pad = 1.0, w_pad = 1.0, h_pad = 1.0)
-    plt.show()
-
-def set_prog(prog="dot"):
-    path = r'C:\Program Files (x86)\Graphviz\bin' # bin directory of graphviz
-    prog = os.path.join(path, prog) + '.exe'
-    return prog
-
 
 if __name__ == "__main__":
 
@@ -65,32 +37,23 @@ if __name__ == "__main__":
     # df1['RecipeIngredientParts'] = df2
 
 
-    # G = nx.Graph()
-    # G.add_nodes_from([(0, {"name_":"Biryani", "type_":"Recipe"}),
-    #                 (1, {"name_":"Best Lemonade", "type_":"Recipe"})])
+    G = nx.Graph()
+    G.add_nodes_from([(0, {"name_":"Biryani", "type_":"Recipe"}),
+                    (1, {"name_":"Best Lemonade", "type_":"Recipe"})])
     
-    # G.add_nodes_from([(2, {"name_":"blueberries", "type_":"Ingredient"}),
-    #                 (3, {"name_":"granulated sugar", "type_":"Ingredient"}),
-    #                 (4, {"name_":"vanilla yogurt", "type_":"Ingredient"}),
-    #                 (5, {"name_":"lemon juice", "type_":"Ingredient"}),
-    #                 (6, {"name_":"peppercorns", "type_":"Ingredient"})])
+    G.add_nodes_from([(2, {"name_":"blueberries", "type_":"Ingredient"}),
+                    (3, {"name_":"granulated sugar", "type_":"Ingredient"}),
+                    (4, {"name_":"vanilla yogurt", "type_":"Ingredient"}),
+                    (5, {"name_":"lemon juice", "type_":"Ingredient"}),
+                    (6, {"name_":"peppercorns", "type_":"Ingredient"})])
 
-    # G.add_edges_from([(0,3),(0,4),(0,5),(1,4),(1,3),(1,6)])
+    G.add_edges_from([(0,3),(0,4),(0,5),(1,4),(1,3),(1,6)])
 
     
-    # nx.draw(G, with_labels=True, font_weight='bold')
-    # plt.axis('equal')
-    # plt.show()
+    nx.draw(G, with_labels=True, font_weight='bold')
+    plt.axis('equal')
+    plt.show()
 
-
-    # import dgl
-    # import dglke
-    # g = dgl.from_networkx(G)
-    # #print(g.nodes())
-    # #print(g.edges())
-
-    # from dgl.data.utils import save_graphs
-    # save_graphs("./data.bin", [g])
 
     # test_directory = Path(__file__).parent / 'dataset'
     # dataset_file = [test_directory/'relations.tsv']
@@ -98,29 +61,3 @@ if __name__ == "__main__":
 
 
     # df1.to_csv("./dataset/test.txt", header = None, index = None, sep = "\t")
-
-    import subprocess
-    # subprocess.run(["dglke_train","--model_name", "TransE_l2",\
-    #             "--batch_size", "1000",\
-    #             "--neg_sample_size", "1", \
-    #             "--hidden_dim", "4", \
-    #             "--gamma", "19.9", \
-    #             "--lr", "0.25", \
-    #             "--max_step", "30", \
-    #             "--log_interval", "10", \
-    #             "--batch_size_eval", "16", \
-    #             "-adv", \
-    #             "--regularization_coef", "1.00E-09", \
-    #             "--save_path", "./data", \
-    #             "--data_path", "./dataset/", \
-    #             "--format", "raw_udd_hrt", \
-    #             "--data_files", "train.tsv", \
-    #             "--dataset", "xxx",\
-    #             "--neg_sample_size_eval", "10000",\
-    #             "--num_thread", "1",\
-    #             "--num_proc", "1"], stdout=subprocess.PIPE)
-
-    subprocess.run(["dglke_emb_sim ",\
-            "--emb_file", "data/TransE_l2_xxx_7/xxx_TransE_l2_entity.npy",\
-            "--format", "l_r", \
-            "--data_files", "head.list", "tail.list"], stdout=subprocess.PIPE)
