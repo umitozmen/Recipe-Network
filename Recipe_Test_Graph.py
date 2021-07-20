ff76116bffab2ba49142c2d3ffe7dbaacfeff3ef
Umit <60366307+umitozmen@users.noreply.github.com>
2021-07-20 20:25:47 +0100

    Network tsv creation

--- Recipe_Test_Graph.py
+++ Recipe_Test_Graph.py
@@ -1,11 +1,9 @@
 from pathlib import Path
-#from mpl_toolkits.mplot3d import Axes3D
-#from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt # plotting
-#import numpy as np # linear algebra
-import os # accessing directory structure
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import networkx as nx
+import glob
+import os
 
 def get_sample_file(nRowsRead=None):
 
@@ -13,20 +11,17 @@ def get_sample_file(nRowsRead=None):
     dataset_file = [main_directory/'recipes.csv']
     col_list = ["RecipeId","Name","RecipeCategory","RecipeIngredientQuantities","RecipeIngredientParts","AggregatedRating","ReviewCount"]
     dtype_dic= { 'RecipeId':int, 'Name':str, 'RecipeCategory':str, 'RecipeIngredientQuantities':str, 'RecipeIngredientParts':str, 'AggregatedRating':float, 'ReviewCount':float }
-    
     #nRowsRead specify 'None' if want to read whole file
     
-    df1 = pd.read_csv(dataset_file[0], delimiter=',', usecols=col_list, dtype = dtype_dic).sample(nRowsRead, random_state=44)
+    df1 = pd.read_csv(dataset_file[0], delimiter=',', usecols=col_list, dtype = dtype_dic)
 
     df1.dataframeName = f'{nRowsRead}_samples.csv'
 
     #df1.to_csv(f"./dataset/{nRowsRead}_samples.csv", index = None, sep = ",")
 
-    return df1 , print(f"./dataset/{nRowsRead}_samples.csv created")
+    return df1 #, print(f"./dataset/{nRowsRead}_samples.csv created")
 
-def create_dlg_ke_relations(df1):
-    
-    col_list_to_tidy = ["Name","RecipeCategory","RecipeIngredientQuantities","RecipeIngredientParts"]
+def data_eng(df1,col_list_to_tidy):
 
     for col in col_list_to_tidy:
         df1[col] = df1[col].str.replace('c("','', regex = False)
@@ -36,46 +31,60 @@ def create_dlg_ke_relations(df1):
         df1[col] = df1[col].str.split(',')
 
     df1.reset_index(drop=True)
+    
+    #df1.to_csv(f"./dataset/{nRowsRead}_samples.csv", index = None, sep = ",")
 
     df_rel = pd.DataFrame(columns = ["head", "rel", "tail"])
 
     for i, rec_val in enumerate(df1["RecipeIngredientParts"]) :
+        if i in [100000,200000,300000,400000,500000]:
+            df_rel.to_csv(f"./dataset/train{i}.tsv", header = None, index = None, sep = "\t")
+            df_rel = pd.DataFrame(columns = ["head", "rel", "tail"])
+
         for ing_val in rec_val:
             df_rel = df_rel.append({"head": ing_val.strip(), "rel" : "ingredient_of", "tail": df1.iloc[i]['Name'][0].strip()}, ignore_index=True)
 
-    #df_rel.to_csv("./dataset/train.tsv", header = None, index = None, sep = "\t")
-
-    return df_rel
+    df_rel.to_csv(f"./dataset/train_last.tsv", header = None, index = None, sep = "\t")
 
 if __name__ == "__main__":
 
-    nRowsRead = 1000
-
+    #nRowsRead = 'all'
+    #col_list_to_tidy = ["Name","RecipeCategory","RecipeIngredientQuantities","RecipeIngredientParts"]
     
-    df1 = get_sample_file(nRowsRead)
+    #df1 = get_sample_file(nRowsRead, col_list_to_tidy)
 
-    df_rel = create_dlg_ke_relations(df1)
+    main_directory = Path(__file__).parent / 'dataset'
+     
+    all_files = glob.glob(os.path.join(main_directory, "*.tsv")) 
 
-    G = nx.Graph()
-
-    for i, ing in enumerate(df_rel['head']):
-        G.add_nodes_from([(f'i{i}', {"name_": ing, "type_":"Ingredient"})])
+    df_from_each_file = (pd.read_csv(f) for f in all_files)
+    concatenated_df   = pd.concat(df_from_each_file, ignore_index=True)
     
-    for i, rec in enumerate(df_rel['tail']):
-        G.add_nodes_from([(f'r{i}', {"name_": rec, "type_":"Recipe"})])
-
-    print(G.number_of_nodes())
-
+    concatenated_df.to_csv(f"./dataset/train_allset.tsv", header = None, index = None, sep = "\t")
     # G = nx.Graph()
-    # G.add_nodes_from([(0, {"name_":"Biryani", "type_":"Recipe"}),
-    #                 (1, {"name_":"Best Lemonade", "type_":"Recipe"})])
-    
 
+    # for i, rec in enumerate(df_rel['tail']):
+    #     G.add_nodes_from([(rec, {"id_": f'r{i}', "type_":"Recipe"})])
 
-    # G.add_edges_from([(0,3),(0,4),(0,5),(1,4),(1,3),(1,6)])
+    # for i, ing in enumerate(df_rel['head']):
+    #     G.add_nodes_from([(ing, {"id_": f'i{i}', "type_":"Ingredient"})])
+    #     G.add_edges_from([(ing,df_rel.iloc[i]['tail'])])
 
-    
-    # nx.draw(G, with_labels=True, font_weight='bold')
+    # print(G.number_of_nodes())
+    # print(G.number_of_edges())
+   
+    # nx.draw(G, with_labels=False)
     # plt.axis('equal')
     # plt.show()
 
+    # import json
+    # from networkx.readwrite import json_graph
+
+    # for n in G:
+    #     G.nodes[n]['name'] = n
+
+    # d = json_graph.node_link_data(G)
+    # json.dump(d, open('force/force.json','w'))
+
+    # from networkx import http_server
+    # http_server.load_url('force/force.html')
